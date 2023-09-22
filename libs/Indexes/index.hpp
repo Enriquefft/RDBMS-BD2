868e7be231f8b2a16b189cdec9cfb13f83cf2c8b
nicolas-castaneda <nicolas.castaneda@utec.edu.pe>
2023-09-22 09:37:05 -0500

    Update - Dependencies add

--- libs/Indexes/index.hpp
+++ libs/Indexes/index.hpp
@@ -1,68 +1,69 @@
-#ifndef INDEX_HPP
-#define INDEX_HPP
+#ifndef INDEX_FILE_HPP
+#define INDEX_FILE_HPP
 
 #include "data.hpp"
 #include "response.hpp"
 #include "utils.hpp"
 
-template <typename KEY_TYPE> class Index {
-protected:
-  std::string attribute_name;
-  std::string table_name;
-  std::string index_name;
-  std::string duplicatesFilename;
-  bool PK;
+namespace Index {
+  template <typename KEY_TYPE> class Index {
+  protected:
+    std::string attribute_name;
+    std::string table_name;
+    std::string index_name;
+    std::string duplicatesFilename;
+    bool PK;
 
-  template <typename RecordType>
-  void getAllRawCurrentRecords(RecordType sir,
-                               std::vector<physical_pos> &records);
+    template <typename RecordType>
+    void getAllRawCurrentRecords(RecordType sir,
+                                std::vector<physical_pos> &records);
 
-  template <typename FileType = std::fstream, typename RecordType>
-  void insertDuplicate(FileType &file, RecordType &sir, RecordType &sir_dup);
+    template <typename FileType = std::fstream, typename RecordType>
+    void insertDuplicate(FileType &file, RecordType &sir, RecordType &sir_dup);
 
-  template <typename RecordType>
-  void insertDuplicateFile(RecordType &sir, physical_pos &duplicate_position);
+    template <typename RecordType>
+    void insertDuplicateFile(RecordType &sir, physical_pos &duplicate_position);
 
-  template <typename HeaderType, typename RecordType>
-  size_t numberRecordsWithHeader(std::string file_name);
+    template <typename HeaderType, typename RecordType>
+    size_t numberRecordsWithHeader(std::string file_name);
 
-  template <typename HeaderType, typename RecordType>
-  void printFileWithHeader(std::string file_name);
+    template <typename HeaderType, typename RecordType>
+    void printFileWithHeader(std::string file_name);
 
-  template <typename RecordType> size_t numberRecords(std::string file_name);
+    template <typename RecordType> size_t numberRecords(std::string file_name);
 
-  template <typename RecordType> void printFile(const std::string &file_name);
+    template <typename RecordType> void printFile(const std::string &file_name);
 
-  template <typename FileType = std::fstream, typename HeaderType>
-  void writeHeader(FileType &file, HeaderType &header);
+    template <typename FileType = std::fstream, typename HeaderType>
+    void writeHeader(FileType &file, HeaderType &header);
 
-  template <typename FileType = std::fstream, typename HeaderType>
-  void readHeader(FileType &file, HeaderType &header);
+    template <typename FileType = std::fstream, typename HeaderType>
+    void readHeader(FileType &file, HeaderType &header);
 
-  template <typename FileType = std::fstream, typename RecordType>
-  void readRecord(FileType &file, RecordType &record);
+    template <typename FileType = std::fstream, typename RecordType>
+    void readRecord(FileType &file, RecordType &record);
 
-  template <typename FileType = std::fstream, typename RecordType>
-  void writeRecord(FileType &file, RecordType &record);
+    template <typename FileType = std::fstream, typename RecordType>
+    void writeRecord(FileType &file, RecordType &record);
 
-  template <typename FileType = std::fstream, typename RecordType>
-  void moveReadRecord(FileType &file, physical_pos &pos, RecordType &record);
+    template <typename FileType = std::fstream, typename RecordType>
+    void moveReadRecord(FileType &file, physical_pos &pos, RecordType &record);
 
-  template <typename FileType = std::fstream, typename RecordType>
-  void moveWriteRecord(FileType &file, physical_pos &pos, RecordType &record);
+    template <typename FileType = std::fstream, typename RecordType>
+    void moveWriteRecord(FileType &file, physical_pos &pos, RecordType &record);
 
-public:
-  std::string get_attribute_name() { return this->attribute_name; }
-  std::string get_table_name() { return this->table_name; }
+  public:
+    std::string get_attribute_name() { return this->attribute_name; }
+    std::string get_table_name() { return this->table_name; }
 
-  virtual std::string get_index_name() = 0;
+    virtual std::string get_index_name() = 0;
 
-  virtual Response add(Data<KEY_TYPE> data, physical_pos raw_pos) = 0;
-  virtual Response search(Data<KEY_TYPE> data) = 0;
-  virtual Response rangeSearch(Data<KEY_TYPE> begin, Data<KEY_TYPE> end) = 0;
-  virtual Response erase(Data<KEY_TYPE> data) = 0;
-};
+    virtual Response add(Data<KEY_TYPE> data, physical_pos raw_pos) = 0;
+    virtual Response search(Data<KEY_TYPE> data) = 0;
+    virtual Response rangeSearch(Data<KEY_TYPE> begin, Data<KEY_TYPE> end) = 0;
+    virtual Response erase(Data<KEY_TYPE> data) = 0;
+  };
+  #include "index.tpp"
+}
 
-#include "index.tpp"
-
-#endif // INDEX_HPP
+#endif // INDEX_FILE_HPP
--- libs/Indexes/record.hpp
+++ libs/Indexes/record.hpp
@@ -1,21 +1,24 @@
-#ifndef RECORD_HPP
-#define RECORD_HPP
+#ifndef RECORD_FILE_HPP
+#define RECORD_FILE_HPP
 
 #include "data.hpp"
 #include "utils.hpp"
 
-template <typename KEY_TYPE> class Record {
-public:
-  Data<KEY_TYPE> data;
+namespace Index {
+  template <typename KEY_TYPE> class Record {
+  public:
+    Data<KEY_TYPE> data;
 
-  physical_pos raw_pos;
-  physical_pos dup_pos;
+    physical_pos raw_pos;
+    physical_pos dup_pos;
 
-  void setData(Data<KEY_TYPE> _data) { this->data = _data; }
+    void setData(Data<KEY_TYPE> _data) { this->data = _data; }
 
-  void setRawPos(physical_pos _raw_pos) { this->raw_pos = _raw_pos; }
+    void setRawPos(physical_pos _raw_pos) { this->raw_pos = _raw_pos; }
 
-  void setDupPos(physical_pos _dup_pos) { this->dup_pos = _dup_pos; }
-};
+    void setDupPos(physical_pos _dup_pos) { this->dup_pos = _dup_pos; }
+  };
+}
 
-#endif // RECORD_HPP
+
+#endif // RECORD_FILE_HPP
